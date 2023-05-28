7988213e456571e0d02aa8976dd85308be42ea25
İbrahim Can <118071108+LegendMan46@users.noreply.github.com>
2023-05-28 22:35:25 +0300

    Delete http_sChangerScrip.py

--- http_sChangerScrip.py
+++
@@ -1,9 +0,0 @@
-import mitmproxy
-
-def request(flow):
-    print(flow)
-    if flow.response.headers.get("content-type", "").startswith("image"):
-        img = open("ihackedyou.png", "rb").read()
-        flow.response.content = img
-        flow.response.headers["content-type"] = "image/png"
-    
